ae0aa912970beaf2164372f06c37d9b62af58c85
ASM0 <ASM0@inbox.ru>
2024-06-02 20:37:49 +0300

    added code for FSM synthesis of the new model

--- tests/test_fsm_inference.py
+++ tests/test_fsm_inference.py
@@ -1,7 +1,9 @@
+from aalpy import run_non_det_Lstar
 from aalpy.base import SUL
 from aalpy.oracles import RandomWalkEqOracle
-from aalpy.learning_algs import run_Lstar
-from objects.hvac.controller import VentilationSystemController, command_handler
+from aalpy.learning_algs import run_Lstar, run_KV
+from systems.hvac.controller import VentilationSystemController, command_handler
+from systems.microfluidic_flow_control.controller import MicrofluidicSystem, microfluidic_system_command_handler
 
 
 class HVACSystemSUL(SUL):
@@ -18,7 +20,6 @@ class HVACSystemSUL(SUL):
     # Этот метод должен моделировать, как система реагирует на каждый вход.
     def step(self, command):
         response = command_handler(self.hvac_system, command) if command else command_handler(self.hvac_system, 'r')
-        print(f"{command = }, {response = }")
         return response
 
     # Очищает или завершает текущую сессию работы с системой.
@@ -27,12 +28,72 @@ class HVACSystemSUL(SUL):
         pass
 
 
-if __name__ == "__main__":
+class MicrofluidicSystemSUL(SUL):
+    def __init__(self):
+        super().__init__()
+        self.microfluidic_system = None
+
+    def pre(self):
+        self.microfluidic_system = MicrofluidicSystem()
+
+    def step(self, command):
+        return microfluidic_system_command_handler(self.microfluidic_system, command)
+
+    def post(self):
+        pass
+
+
+def fsm_hvac_system_model():
     alphabet = ['l', 'ld', 'Mc', 'Mh', 'Mf', 'Md', 'SMt', 'SF', 'SH', 'ST',
-                's3', 's2', 's1', 's0', 'r']  # Определение алфавита
-    sul = HVACSystemSUL()  # Создание SUL (System Under Learning)
+                's3', 's2', 's1', 's0', 'r']
+    sul = HVACSystemSUL()
     eq_oracle = RandomWalkEqOracle(alphabet=alphabet, sul=sul, num_steps=1000,
-                                   reset_prob=0.1)  # Определение оракула эквивалентности
+                                   reset_prob=0.1)
     learned_model = run_Lstar(alphabet=alphabet, sul=sul, eq_oracle=eq_oracle,
-                              automaton_type='dfa')  # Запуск алгоритма L*
-    print(learned_model)  # Вывод изученного конечного автомата
+                           automaton_type='dfa')
+    learned_model.save("HVACLearnedModel")
+
+
+def fsm_microfluidic_system_model():
+    alphabet = ['fill', 'next', 'empty']
+    sul = MicrofluidicSystemSUL()
+    eq_oracle = RandomWalkEqOracle(alphabet=alphabet, sul=sul, num_steps=1000,
+                                   reset_prob=0.1)
+    learned_model = run_non_det_Lstar(alphabet=alphabet, sul=sul, eq_oracle=eq_oracle)
+    learned_model.save("Microfluidic_non_det_Lstar")
+
+    sul = MicrofluidicSystemSUL()
+    eq_oracle = RandomWalkEqOracle(alphabet=alphabet, sul=sul, num_steps=1000,
+                                   reset_prob=0.1)
+    learned_model = run_Lstar(alphabet=alphabet, sul=sul, eq_oracle=eq_oracle, automaton_type='dfa')
+    learned_model.save("Microfluidic_Lstar_dfa")
+
+    sul = MicrofluidicSystemSUL()
+    eq_oracle = RandomWalkEqOracle(alphabet=alphabet, sul=sul, num_steps=1000,
+                                   reset_prob=0.1)
+    learned_model = run_Lstar(alphabet=alphabet, sul=sul, eq_oracle=eq_oracle, automaton_type='moore')
+    learned_model.save("Microfluidic_Lstar_moore")
+
+    sul = MicrofluidicSystemSUL()
+    eq_oracle = RandomWalkEqOracle(alphabet=alphabet, sul=sul, num_steps=1000,
+                                   reset_prob=0.1)
+    learned_model = run_Lstar(alphabet=alphabet, sul=sul, eq_oracle=eq_oracle, automaton_type='mealy')
+    learned_model.save("Microfluidic_Lstar_mealy")
+
+    sul = MicrofluidicSystemSUL()
+    eq_oracle = RandomWalkEqOracle(alphabet=alphabet, sul=sul, num_steps=1000,
+                                   reset_prob=0.1)
+    learned_model = run_KV(alphabet=alphabet, sul=sul, eq_oracle=eq_oracle, automaton_type='moore')
+    learned_model.save("Microfluidic_KV_moore")
+
+
+    sul = MicrofluidicSystemSUL()
+    eq_oracle = RandomWalkEqOracle(alphabet=alphabet, sul=sul, num_steps=1000,
+                                   reset_prob=0.1)
+    learned_model = run_KV(alphabet=alphabet, sul=sul, eq_oracle=eq_oracle, automaton_type='mealy')
+    learned_model.save("Microfluidic_KV_mealy")
+
+
+if __name__ == "__main__":
+    #fsm_hvac_system_model()
+    fsm_microfluidic_system_model()
