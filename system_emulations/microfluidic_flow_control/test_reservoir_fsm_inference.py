41b3ca61868037e8cd72e822c24975be50476989
Sheeshelty <87362691+Sheslty@users.noreply.github.com>
2024-06-06 11:02:17 +0300

    Add files via upload

---
+++ system_emulations/microfluidic_flow_control/test_reservoir_fsm_inference.py
@@ -0,0 +1,43 @@
+from aalpy import run_non_det_Lstar
+from aalpy.oracles import RandomWalkEqOracle
+from aalpy.learning_algs import run_Lstar
+
+from system_emulations.test_locators import PATH_TO_RESULTS_DIR
+from system_emulations.test_sul import MicrofluidicSystemSUL
+
+
+def main():
+    alphabet = ['fill', 'next', 'empty']
+
+    sul = MicrofluidicSystemSUL()
+    eq_oracle = RandomWalkEqOracle(alphabet=alphabet, sul=sul, num_steps=1000,
+                                   reset_prob=0.1)
+    learned_model = run_non_det_Lstar(alphabet=alphabet, sul=sul,
+                                      eq_oracle=eq_oracle)
+    learned_model.save(PATH_TO_RESULTS_DIR + "Microfluidic_non_det_L")
+
+    sul = MicrofluidicSystemSUL()
+    eq_oracle = RandomWalkEqOracle(alphabet=alphabet, sul=sul, num_steps=1000,
+                                   reset_prob=0.1)
+    learned_model = run_Lstar(alphabet=alphabet, sul=sul, eq_oracle=eq_oracle,
+                              automaton_type='dfa')
+    learned_model.save(PATH_TO_RESULTS_DIR + "Microfluidic_L_dfa")
+
+    sul = MicrofluidicSystemSUL()
+    eq_oracle = RandomWalkEqOracle(alphabet=alphabet, sul=sul, num_steps=1000,
+                                   reset_prob=0.1)
+    learned_model = run_Lstar(alphabet=alphabet, sul=sul, eq_oracle=eq_oracle,
+                              automaton_type='moore')
+    learned_model.save(PATH_TO_RESULTS_DIR + "Microfluidic_L_moore")
+
+    sul = MicrofluidicSystemSUL()
+    eq_oracle = RandomWalkEqOracle(alphabet=alphabet, sul=sul, num_steps=1000,
+                                   reset_prob=0.1)
+    learned_model = run_Lstar(alphabet=alphabet, sul=sul, eq_oracle=eq_oracle,
+                              automaton_type='mealy')
+    learned_model.save(PATH_TO_RESULTS_DIR + "Microfluidic_L_mealy")
+
+
+if __name__ == "__main__":
+    main()
+
